c014ced8895475ab3428ecfa0483e1ed9fbb87cb
Saraja2607 <66618764+Saraja2607@users.noreply.github.com>
2021-07-19 14:05:33 +0530

    Delete CMT_CoP directory

--- CMT_CoP/DDL/Users.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE USER Jenkins_user PASSWORD = 'hello'  MUST_CHANGE_PASSWORD = FALSE;
-
--- Granting Security role to the user 
-GRANT ROLE SYSADMIN,SECURITYADMIN TO USER Jenkins_user;
--- CMT_CoP/Privileges/Grant.sql
+++
@@ -1,2 +0,0 @@
-	-- Granting Security role to the user 
-GRANT ROLE SYSADMIN,SECURITYADMIN TO USER Jenkins_user;
