6464bff38d50ea17c051d3e086c7bd67d168bf19
Saraja2607 <66618764+Saraja2607@users.noreply.github.com>
2021-08-03 17:38:12 +0530

    Create schema_creation.sql

---
+++ cmt/ddl/schemas/schema_creation.sql
@@ -0,0 +1,13 @@
+
+use compute_wh;
+
+
+create schema cmt_dev.cmt_hr;
+create schema cmt_beta.cmt_hr;
+create schema cmt_prod.cmt_hr;
+
+create schema cmt_dev.cmt_finance;
+create schema cmt_beta.cmt_finance;
+
+
+
